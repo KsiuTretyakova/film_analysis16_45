2cfb3334ad9c48724b0c51cde3bec1a53261a862
KsiuTretyakova <hrytsenkoks@gmail.com>
2024-11-23 18:22:02 +0200

    Stated write a doc about dataset

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn
+import seaborn as sns
 import ast
 
 # download dataset
@@ -9,8 +9,8 @@ url = "movies_metadata.csv"
 movies_df = pd.read_csv(url)
 
 # output info about dataset
-# movies_df.info()
-# print(movies_df.head())
+movies_df.info()
+print(movies_df.head())
 
 # print(movies_df.describe())
 
@@ -43,8 +43,39 @@ movies_df.dropna(subset=['budget', 'revenue'], inplace=True)
 # --------------------------------
 
 # print(movies_df['release_date'])
-movies_df['release_date'] = pd.to_datetime(movies_df['release_date'], errors='coerce').dt.year
-# print(movies_df['release_date'])
+movies_df['release_year'] = pd.to_datetime(movies_df['release_date'], errors='coerce').dt.year
+# print(movies_df['release_year'])
 
 # ------------------------------------------------
 
+genres_exploded = movies_df[['title', 'release_year', 'budget', 'revenue', 'genres']].explode('genres')
+print(genres_exploded)
+
+genre_count = genres_exploded['genres'].value_counts()
+print(genre_count)
+
+# Візуазізація жанрів
+plt.figure(figsize=(10,6))
+sns.barplot(x=genre_count.index, y=genre_count.values)
+plt.title("Кількість фільмів за жанрами")
+plt.xlabel("Жанр")
+plt.ylabel("Кількість фільмів")
+plt.xticks(rotation=45)
+plt.tight_layout()
+# plt.show()
+
+
+# ------------------------------------------------
+movies_df['popularity'] = pd.to_numeric(movies_df['popularity'], errors='coerce')
+popularity_trend = movies_df.groupby('release_year')['popularity'].mean()
+
+# Візуазізація трендів популярності
+
+plt.figure(figsize=(12,6))
+popularity_trend.plot()
+plt.title("Середня популярність фільмів за роками")
+plt.xlabel("Рік")
+plt.ylabel("Популярність")
+# plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
